59cbe86cacd6738b5e97276065e80610b0effafa
weetok <weetok@live.nl>
2015-06-18 15:08:07 +0200

    Added connectdb

---
+++ De Hondsrug/Includes/connectdb.php
@@ -0,0 +1,16 @@
+<?php
+ // 1. Verbinding maken met database
+ $dbhost = "localhost";
+ $dbuser = "pellebra";
+ $dbpass = "nf8SRau490";
+ $dbname = "pellebra_Eat-IT";
+ $connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+ $db = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+ //Foutmelding wanneer connectie niet lukt
+ if(mysqli_connect_errno()) {
+ 	die("Database connection failed: " .
+ 		mysqli_connect_error() . " (" .
+ 		mysqli_connect_errno() . ") "
+ 	);
+ }
+?>
