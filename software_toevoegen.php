64369e78fb038579da96313d81037d999f7896e9
Frank Mulder <Frank Mulder@Frank.dynamic.ziggo.nl>
2015-06-19 13:41:10 +0200

    software toevoegen is ook waterdicht

--- software_toevoegen.php
+++ software_toevoegen.php
@@ -8,11 +8,30 @@
     $titles = mysqli_fetch_assoc($result);
     
     if(isset($_POST['opslaan'])):
-        $insert = "INSERT INTO software (sw_id,uitgebreidde_naam,soort,producent,leverancier,aantal_licenties,serverlicentie,aantal_gebruikers) "
-            . "VALUES ('".$_POST['sw_id']."','".$_POST['uitgebreidde_naam']."','".$_POST['soort']."','".$_POST['producent']."','".$_POST['leverancier']."','".$_POST['aantal_licenties']."','".$_POST['serverlicentie']."','".$_POST['aantal_gebruikers']."')";
-        mysqli_query($db, $insert);
-        header('Location: software.php');
-        exit;
+        if(!empty($_POST['sw_id']) && !empty($_POST['uitgebreidde_naam']) && !empty($_POST['soort']) && !empty($_POST['producent']) && !empty($_POST['leverancier']) && !empty($_POST['aantal_licenties']) && !empty($_POST['serverlicentie'])):
+            if(is_numeric($_POST['aantal_licenties'])):
+                if(is_numeric($_POST['serverlicentie'])):
+                    if(empty($_POST['aantal_gebruikers'])):
+                        $_POST['aantal_gebruikers'] = 0;
+                    endif;
+                    if(is_numeric($_POST['aantal_gebruikers'])):
+                        $insert = "INSERT INTO software (sw_id,uitgebreidde_naam,soort,producent,leverancier,aantal_licenties,serverlicentie,aantal_gebruikers) "
+                            . "VALUES ('".$_POST['sw_id']."','".$_POST['uitgebreidde_naam']."','".$_POST['soort']."','".$_POST['producent']."','".$_POST['leverancier']."','".$_POST['aantal_licenties']."','".$_POST['serverlicentie']."','".$_POST['aantal_gebruikers']."')";
+                        mysqli_query($db, $insert);
+                        header('Location: software.php');
+                        exit;
+                    else:
+                        echo "Aantal gebruikers dient een getal te zijn.";
+                    endif;
+                else:
+                    echo "Serverlicentie dient een getal te zijn.";
+                endif;
+            else:
+                echo "Aantal licenties dient een getal te zijn.";
+            endif;
+        else:
+            echo "U heeft nog niet alle verplichte velden ingevoerd.";
+        endif;
     endif;
     
     if(isset($_POST['overzicht'])):
