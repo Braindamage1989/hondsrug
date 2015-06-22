67c6f95fad2e0a395cb24ebd00c8b187cbc851fb
Frank Mulder <Frank Mulder@Frank>
2015-06-22 20:24:49 +0200

    php files een of andere manier niet gepushed

--- hardware.php
+++ hardware.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
-
+    
+    require_once 'includes/header.html';
     require_once 'includes/connectdb.php';
     
     $query_all = "SELECT * FROM hardware";
@@ -22,36 +23,47 @@
         exit;
     endif;
 ?>
-<body>
-    <form action="" method="POST">
-        <table>
-            <tr>
-                <td></td>
-                <?php
-                    foreach($titles as $k => $v):
-                        echo "<td><b>$k</b></td>\n";
-                    endforeach;
-                ?>
-            </tr>
-            <?php
-                while($row = mysqli_fetch_assoc($result_all)):
-            ?>
+<div class="titel2">
+    <div class="container">
+        <h1>Hardware</h1>
+    </div>
+</div>
+<div class="lijst">
+    <div class="container">
+        
+        <form action="" method="POST">
+            <table>
                 <tr>
-            <?php
-                    foreach($row as $k => $v):
-                        if($k == 'hw_id'):
-                            echo "<td><input type=\"checkbox\" name=\"id[]\" value=\"$v\"></td>\n";
-                        endif;
-                        echo "<td>$v</td>\n";
-                    endforeach;
-            ?>
+                    <td></td>
+                    <?php
+                        foreach($titles as $k => $v):
+                            echo "<td><b>$k</b></td>\n";
+                        endforeach;
+                    ?>
                 </tr>
-            <?php
-                endwhile;
-            ?>
-            
-        </table>
-        <input type="submit" name="toevoegen" value="Toevoegen" />
-        <input type="submit" name="inline" value="Bewerk" />
-    </form>
-</body>
+                <?php
+                    while($row = mysqli_fetch_assoc($result_all)):
+                ?>
+                    <tr>
+                <?php
+                        foreach($row as $k => $v):
+                            if($k == 'hw_id'):
+                                echo "<td><input type=\"checkbox\" name=\"id[]\" value=\"$v\"></td>\n";
+                            endif;
+                            echo "<td>$v</td>\n";
+                        endforeach;
+                ?>
+                    </tr>
+                <?php
+                    endwhile;
+                ?>
+
+            </table>
+            <input type="submit" name="toevoegen" value="Toevoegen" />
+            <input type="submit" name="inline" value="Bewerk" />
+        </form>
+    </div>
+</div>
+<?php 
+    require_once 'includes/header.html'; 
+?>
