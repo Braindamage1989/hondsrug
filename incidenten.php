0c680c1a8d38d2e01bff459838e028d0b7dd2dd2
Frank Mulder <Frank Mulder@Frank.dynamic.ziggo.nl>
2015-06-22 11:53:59 +0200

    css is nu heel mooi

--- incidenten.php
+++ incidenten.php
@@ -2,6 +2,7 @@
     session_start();
 
     require_once 'includes/connectdb.php';
+    require_once 'includes/header.html';
     
     $query_all = "SELECT id, omschrijving, datum, starttijd, hw_id, sw_id, toegekend_aan, melder FROM incidenten";
     $result_all = mysqli_query($db, $query_all);
@@ -19,36 +20,39 @@
         exit;
     endif;
 ?>
-<body>
-    <form action="" method="POST">
-        <table>
-            <tr>
-                <td></td>
-                <?php
-                    foreach($titles as $k => $v):
-                        echo "<td><b>$k</b></td>\n";
-                    endforeach;
-                ?>
-            </tr>
-            <?php
-                while($row = mysqli_fetch_assoc($result_all)):
-            ?>
-                <tr>
-            <?php
-                    foreach($row as $k => $v):
-                        if($k == 'id'):
-                            echo "<td><input type=\"checkbox\" name=\"id[]\" value=\"$v\"></td>\n";
-                        endif;
-                        echo "<td>$v</td>\n";
-                    endforeach;
-            ?>
-                </tr>
-            <?php
-                endwhile;
-            ?>
-            
-        </table>
-        <input type="submit" name="inline" value="Bewerk" />
-        <input type="submit" name="detail" value="Toon details" />
-    </form>
-</body>
+    <div class="lijst">
+        <div class="container">
+            <form action="" method="POST">
+                <table>
+                    <tr>
+                        <td></td>
+                        <?php
+                            foreach($titles as $k => $v):
+                                echo "<td><b>$k</b></td>\n";
+                            endforeach;
+                        ?>
+                    </tr>
+                    <?php
+                        while($row = mysqli_fetch_assoc($result_all)):
+                    ?>
+                        <tr>
+                    <?php
+                            foreach($row as $k => $v):
+                                if($k == 'id'):
+                                    echo "<td><input type=\"checkbox\" name=\"id[]\" value=\"$v\"></td>\n";
+                                endif;
+                                echo "<td>$v</td>\n";
+                            endforeach;
+                    ?>
+                        </tr>
+                    <?php
+                        endwhile;
+                    ?>
+
+                </table>
+                <input type="submit" name="inline" value="Bewerk" />
+                <input type="submit" name="detail" value="Toon details" />
+            </form>
+        </div>
+    </div>
+<?php require_once 'includes/header.html'; ?>
