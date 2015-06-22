b8b43689c29160f1bc8729777c879929a559b012
Frank Mulder <Frank Mulder@Frank>
2015-06-22 20:37:23 +0200

    gebruikers_inline.php erbij

---
+++ gebruikers_inline.php
@@ -0,0 +1,77 @@
+<?php
+    session_start();
+    require_once 'includes/connectdb.php';
+    
+    $ids = implode(' OR id = ', $_SESSION['ids']);
+    
+    $query = "SELECT * FROM gebruikers WHERE id = $ids";
+    $result = mysqli_query($db, $query);
+    
+    if(isset($_POST['opslaan'])):
+        foreach ($_SESSION['ids'] as $id):
+            $update = "UPDATE gebruikers SET voornaam='".$_POST[$id][1]."', "
+            . "achternaam='".$_POST[$id][2]."', "
+            . "email='".$_POST[$id][3]."', "
+            . "wachtwoord='".$_POST[$id][4]."', "
+            . "functie='".$_POST[$id][5]."', "
+            . "status='".$_POST[$id][6]."'"
+            . " WHERE id='".$_POST[$id][0]."'";
+            mysqli_query($db, $update);
+            echo $update;
+            echo "<br/>";
+        endforeach;
+        empty($SESSION['ids']);
+        //header('Location: gebruikers.php');
+        exit;
+    endif;
+    
+    if(isset($_POST['overzicht'])):
+        header('Location: gebruikers.php');
+        exit;
+    endif;
+?>
+<body>
+    <form action="" method="POST">
+        <table>
+            <tr>
+                <td><b>ID</b></td>
+                <td><b>Voornaam</b></td>
+                <td><b>Achternaam</b></td>
+                <td><b>E-mail</b></td>
+                <td><b>Wachtwoord</b></td>
+                <td><b>Functie</b></td>
+                <td><b>Status</b></td>
+            </tr>
+            <?php
+                while($row = mysqli_fetch_assoc($result)):
+            ?>
+                <tr>
+            <?php
+                    foreach($row as $key => $value) :
+                        if ($key == "functie") :
+                            ?>
+                            <td>
+                                <select name="<?php echo $row["id"] ?>[]">
+                                    <option value="Medewerker IT">Medewerker IT</option>
+                                    <option value="Leerling">Leerling</option>
+                                    <option value="Docent">Docent</option>
+                                </select>
+                            </td>
+                            <?php
+                        elseif ($key == "id") :
+                            echo "<td><input type=\"text\" name=\"".$row["id"]."[]\" value=\"$value\" readonly=\"readonly\"/></td>\n";
+                        else:
+                            echo "<td><input type=\"text\" name=\"".$row["id"]."[]\" value=\"$value\"/></td>\n";
+                        endif;
+                    endforeach;
+            ?>
+                </tr>
+            <?php
+                endwhile;
+            ?>
+            
+        </table>
+        <input type="submit" name="opslaan" value="Opslaan" />
+        <input type="submit" name="overzicht" value="Terug naar overzicht" /> 
+    </form>
+</body>
