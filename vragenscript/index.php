035b8301caa8aa3e685f1ed25687dbd9298b26ab
Frank Mulder <Frank Mulder@Frank>
2015-06-23 16:24:34 +0200

    index voor vragenscript

---
+++ vragenscript/index.php
@@ -0,0 +1,32 @@
+<?php
+    require_once 'includes/header.html';
+?>
+
+<div class="titel1">
+  <div class="container">
+    <h1>Heeft u een probleem met onze ICT-dienst?</h1>
+    <p>Via deze site kunt u uw probleem bij ons melden</p>
+    <a href="#">Log a.u.b. eerst in</a>
+  </div>
+</div> 
+
+<div class="learn-more">
+      <div class="container">
+            <div class="row">
+          <div class="col-md-6">
+                    <h3>Meld uw probleem</h3>
+                    <p>Aan de hand van de volgende vragen kunnen wij uw probleem vaststellen en zo spoedig mogelijk een oplossing bieden.</p>
+                    <p><a href="incidenten.php">Ga naar de vragen</a></p>
+          </div>
+              <div class="col-md-6">
+                    <h3>F.A.Q.</h3>
+                    <p>Bekijk onze F.A.Q. om te kijken of uw vraag hiertussen staat.</p>
+                    <p><a href="problemen.php">Ga naar de F.A.Q.</a></p>
+              </div>
+        </div>
+    </div>
+</div>
+
+<?php
+    require_once 'includes/footer.html';
+?>
