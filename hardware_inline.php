93772645f760839cf510371e452c5d1227b40459
Frank Mulder <Frank Mulder@Frank>
2015-06-23 14:02:41 +0200

    hardware inline met css

--- hardware_inline.php
+++ hardware_inline.php
@@ -48,50 +48,55 @@
     </div>
 </div>
 <div class="lijst">
-    <div class="container">
-        <form action="" method="POST">
-            <table>
-                <tr>
-                    <?php
-                        foreach($titles as $k => $v):
-                            echo "<td><b>$k</b></td>\n";
-                        endforeach;
-                    ?>
-                </tr>
-                <?php
-                    while($row = mysqli_fetch_assoc($result_all)):
-                ?>
+    <div class="container-fluid">
+        <div class="col-md-10">
+            <form action="" method="POST">
+                <table>
                     <tr>
-                <?php
-                        foreach($row as $k => $v):
-                            if($k == 'hw_id') :
-                                echo "<td><input type=\"text\" readonly=\"readonly\" name=\"".$row["hw_id"]."[]\" value=\"$v\"/></td>\n";
-                            elseif($k == 'connected_hw') :
-                                echo"<td><select name=\"".$row["hw_id"]."[]\">";
-                                while($hw_id = mysqli_fetch_assoc($dropdown_connected_hw)):
-                                    if($v == $hw_id['hw_id']) :
-                                        echo "<option value=\"".$hw_id['hw_id']."\" selected>".$hw_id['hw_id']."</option>\n";
-                                    else:
-                                        echo "<option value=\"".$hw_id['hw_id']."\">".$hw_id['hw_id']."</option>\n";
-                                    endif;
-                                endwhile;
-                                echo"</select></td>";
-                            else:
-                                echo "<td><input type=\"text\" name=\"".$row["hw_id"]."[]\" value=\"$v\"/></td>\n";
-                            endif;
-                        endforeach;
-                ?>
+                        <?php
+                            foreach($titles as $k => $v):
+                                echo "<td><b>$k</b></td>\n";
+                            endforeach;
+                        ?>
                     </tr>
-                <?php
-                    endwhile;
-                ?>
-
-            </table>
-            <input type="submit" name="opslaan" value="Opslaan" />
-            <input type="submit" name="overzicht" value="Terug naar overzicht" /> 
-    </form>
-</div>
+                    <?php
+                        while($row = mysqli_fetch_assoc($result_all)):
+                    ?>
+                        <tr>
+                    <?php
+                            foreach($row as $k => $v):
+                                if($k == 'hw_id') :
+                                    echo "<td><input type=\"text\" readonly=\"readonly\" name=\"".$row["hw_id"]."[]\" value=\"$v\"/></td>\n";
+                                elseif($k == 'connected_hw') :
+                                    echo"<td><select name=\"".$row["hw_id"]."[]\">";
+                                    while($hw_id = mysqli_fetch_assoc($dropdown_connected_hw)):
+                                        if($v == $hw_id['hw_id']) :
+                                            echo "<option value=\"".$hw_id['hw_id']."\" selected>".$hw_id['hw_id']."</option>\n";
+                                        else:
+                                            echo "<option value=\"".$hw_id['hw_id']."\">".$hw_id['hw_id']."</option>\n";
+                                        endif;
+                                    endwhile;
+                                    echo"</select></td>";
+                                else:
+                                    echo "<td><input type=\"text\" name=\"".$row["hw_id"]."[]\" value=\"$v\"/></td>\n";
+                                endif;
+                            endforeach;
+                    ?>
+                        </tr>
+                    <?php
+                        endwhile;
+                    ?>
+                </table>
+        </div>
+        <div class='col-md-2'>
+            <div class='submenu'>
+                <input type="submit" name="opslaan" value="Opslaan" class="btn btn-primary"/>
+                <input type="submit" name="overzicht" value="Terug naar overzicht" class="btn btn-default"/>
+            </div>
+            </form>
+        </div>
     </div>
+</div>
 <?php 
     require_once 'includes/header.html'; 
 ?>
