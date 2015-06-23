76b0520d7179f6cf93ee0348d01e593586e56c66
Frank Mulder <Frank Mulder@Frank>
2015-06-23 14:29:16 +0200

    problemen css

--- problemen.php
+++ problemen.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
+
     require_once 'includes/connectdb.php';
+    require_once 'includes/header.html';
     
     $query_problemen = "SELECT * FROM problemen";
     $result_problemen = mysqli_query($db, $query_problemen);
@@ -32,51 +34,66 @@
         endforeach;
     endif;
 ?>
-<body>
-    <form action="" method="POST">
-        <table>
-            <tr>
-                <td></td>
-                <td><b>ID</b></td>
-                <td><b>Omschrijving</b></td>
-                <td><b>Known Error</b></td>
-                <td><b>Toegekend aan</b></td>
-                <td><b>Workaround</b></td>
-                <td><b>Status</b></td>
-                <td><b>Incidentnummers</b></td>
-            </tr>
-            <?php
-                while($row = mysqli_fetch_assoc($result_problemen)):
-            ?>
-                <tr>
-            <?php
-                    foreach($row as $k => $v):
-                        echo "<td>$v</td>";
-                    endforeach;
-                    
-                    echo "<td>";
-                    
-                    $query_incidenten = "SELECT inc_id FROM incidenten_probleem WHERE pro_id=".$row['id']."";
-                    $result_incidenten = mysqli_query($db, $query_incidenten);
-                    
-                    while($incidenten = mysqli_fetch_assoc($result_incidenten)):
-                        $aantal = count($incidenten);
-                        foreach($incidenten as $k => $v):
-                                echo "$v,";
-                        endforeach;
-                    endwhile;
-                    
-                    echo "</td>";
-            ?>
-                </tr>
-            <?php
-                endwhile;
-            ?>
-            
-        </table>
-        <input type="submit" name="toevoegen" value="Toevoegen" />
-        <input type="submit" name="inline" value="Bewerk" />
-        <input type="submit" name="verwijderen" value="Verwijderen" />
-        <br />Klik op omschrijving om details van incident te tonen/bewerken
-    </form>
-</body>
+ <div class="titel2">
+    <div class="container">
+        <h1>Problemen</h1>
+    </div>
+</div>
+<div class="lijst">
+    <div class="container">
+        <div class="col-md-11">
+            <form action="" method="POST">
+                <table class="table">
+                    <tr>
+                        <td></td>
+                        <td><b>ID</b></td>
+                        <td><b>Omschrijving</b></td>
+                        <td><b>Known Error</b></td>
+                        <td><b>Toegekend aan</b></td>
+                        <td><b>Workaround</b></td>
+                        <td><b>Status</b></td>
+                        <td><b>Incidentnummers</b></td>
+                    </tr>
+                    <?php
+                        while($row = mysqli_fetch_assoc($result_problemen)):
+                    ?>
+                        <tr>
+                    <?php
+                            foreach($row as $k => $v):
+                                echo "<td>$v</td>";
+                            endforeach;
+
+                            echo "<td>";
+
+                            $query_incidenten = "SELECT inc_id FROM incidenten_probleem WHERE pro_id=".$row['id']."";
+                            $result_incidenten = mysqli_query($db, $query_incidenten);
+
+                            while($incidenten = mysqli_fetch_assoc($result_incidenten)):
+                                $aantal = count($incidenten);
+                                foreach($incidenten as $k => $v):
+                                        echo "$v,";
+                                endforeach;
+                            endwhile;
+
+                            echo "</td>";
+                    ?>
+                        </tr>
+                    <?php
+                        endwhile;
+                    ?>
+                </table>
+        </div>
+        <div class='col-md-1'>
+            <div class='submenu'>
+                <input type="submit" name="toevoegen" value="Toevoegen" class="btn btn-primary"/>
+                <input type="submit" name="inline" value="Bewerk" class="btn btn-default"/>
+                <input type="submit" name="verwijderen" value="Verwijderen" class="btn btn-default"/>
+                <br />Klik op omschrijving om details van incident te tonen/bewerken
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<?php 
+    require_once 'includes/header.html'; 
+?>
